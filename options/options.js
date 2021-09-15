b9f89ac50fd6964297380df4d00dd871e4d266cb
Crowdbotics <team@crowdbotics.com>
2021-09-15 17:50:16 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "fariafitapp_30579Identifier",
+  url: "https://fariafitapp_30579Identifier.botics.co",
+  api: "https://fariafitapp_30579Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
